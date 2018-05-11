bc63ef41834050f652eac29f56f8db7e84a24cb0
asm32 <asm32@qq.com>
2018-05-11 23:21:20 +0800

    ArrayDemo1.scala ... add

---
+++ Scala-2018/Scala-2018-training/ArrayDemo1.scala/ArrayDemo1--exec.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set strCmd=scala ArrayDemo1
+
+echo #%strCmd%
+call %strCmd%
+
+pause
---
+++ Scala-2018/Scala-2018-training/ArrayDemo1.scala/ArrayDemo1--linker.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set strCmd=scalac ArrayDemo1.scala
+
+echo #%strCmd%
+call %strCmd%
+
+pause
