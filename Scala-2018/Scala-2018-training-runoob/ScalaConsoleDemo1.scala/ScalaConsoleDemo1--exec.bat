554e0448686633dc581596634e96f068452fbeda
asm32 <asm32@qq.com>
2018-05-11 23:16:37 +0800

    ScalaConsoleDemo1.scala ... add

---
+++ Scala-2018/Scala-2018-training-runoob/ScalaConsoleDemo1.scala/ScalaConsoleDemo1--exec.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set strCmd=scala ScalaConsoleDemo1
+
+echo #%strCmd%
+call %strCmd%
+
+pause
---
+++ Scala-2018/Scala-2018-training-runoob/ScalaConsoleDemo1.scala/ScalaConsoleDemo1.scala
@@ -0,0 +1,10 @@
+// ScalaConsoleDemo1.scala
+
+object ScalaConsoleDemo1{
+	def main(args: Array[String]){
+		println("请输入菜鸟教程官网: ");
+		val line = Coneole.readLine;
+
+		println("谢谢,你输入的是: " + line);
+	}
+}
